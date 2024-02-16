b6d40f351d559a1400ce403f40aa31fb0e883b34
HesperisNivea <143076071+HesperisNivea@users.noreply.github.com>
2024-02-16 14:06:53 +0100

    update

--- script/Product.js
+++ script/Product.js
@@ -1,6 +1,6 @@
-const salad1 = {name:"ExtraSalad" ,type:"salad", ingredients:[ "ris", "salad", "majs", "sås"], price: 140, description: "Bra salad", img:'C:\Users\radom\OneDrive\Dokumenty\GitHub\LabbFrontEnd\Images\test.jpg'};
-const salad2 = {name:"ExtraSalad" ,type:"salad", ingredients:[ "ris", "salad", "majs", "sås"], price: 140, description: "Bra salad" , img:"..."};
-const salad3 = {name:"ExtraSalad" ,type:"salad", ingredients:[ "ris", "salad", "majs", "sås"], price: 140, description: "Bra salad" , img:"..."};
+const salad1 = {name:"Sommarsalad" ,type:"salad", ingredients:[ "ris", "salad", "majs", "sås"], price: 139, description: "Bra salad", img:'../Images/food-photographer-Sekm9_nC2BM-unsplash.jpg'};
+const salad2 = {name:"Höstsalad" ,type:"salad", ingredients:[ "ris", "salad", "majs", "sås"], price: 116, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard " , img:"../Images/food-photographer-FtpgFeUQuAY-unsplash.jpg"};
+const salad3 = {name:"Wintersalad" ,type:"salad", ingredients:[ "ris", "salad", "majs", "sås"], price: 89, description: "Bra salad" , img:"../Images/food-photographer-zhkhwGrqilw-unsplash.jpg"};
 
 const saladList = [salad1,salad2,salad3]
 
@@ -32,19 +32,18 @@ const mainDiv = document.getElementById("product-container");
 // price.classList.add("text-body-secondary");
 
 for (const salad of saladList) {
-    mainDiv.innerHTML +=`<div class="col">
+    mainDiv.innerHTML +=`<div class="col shop-item">
     <div class="card h-100">
-      <img src="../Images/test.jpg" alt="salad" />
+      <img src="${salad.img}" class="card-img-top shop-item-image" alt="salad" />
       <div class="card-body">
-        <h5 class="card-title">${salad.name}</h5>
+        <div class="d-flex" style="justify-content: space-between;"> <!--remove style and add it to css file-->
+          <h4 class="card-title shop-item-title">${salad.name}</h4>
+          <h5 class="card-title text-end shop-item-price">${salad.price}</h5>
+        </div>
         <p class="card-text">
-          ${salad.description}
+         ${salad.description}
         </p>
-      </div>
-      <div class="card-footer">
-        <small class="text-body-secondary"
-          >${salad.price}</small
-        >
+        <button class="btn btn-success shop-item-button">Add to cart</button>
       </div>
     </div>
   </div>`
